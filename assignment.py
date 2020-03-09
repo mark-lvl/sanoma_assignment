33281b430bd8f0b36b2492e819caedd27d2bc53c
Mark <markteams@gmail.com>
2020-03-10 01:26:05 +0200

    avg_arrival_time method added.

--- assignment.py
+++ assignment.py
@@ -1,4 +1,5 @@
-import datetime
+from datetime import (
+    datetime, date, timedelta)
 import calendar
 import requests
 from pathlib import Path
@@ -24,16 +25,32 @@ def return_date_range(year: int, month: int) -> datetime:
     """Return all days date within the given month"""
     num_days = calendar.monthrange(year, month)[1]
     for day in range(1, num_days+1):
-        yield datetime.date(year, month, day)
+        yield date(year, month, day)
+
+
+def _file_path_generator(filename: str) -> str:
+    """Construct file path in disk"""
+    return Path(f"{OUTPUT_DIR}/{filename}/")
 
 
 def save_to_disk(input: pd.DataFrame, filename: str):
-    output_path = Path(f"{OUTPUT_DIR}/{filename}/")
+    """Export dataframe to disk"""
+    output_path = _file_path_generator(filename)
     log.info(f'Exporting data into {output_path}')
     try:
         input.to_csv(output_path, index=False)
     except Exception as ex:
         log.error(f'Faild on saving file in {output_path}, {ex}')
+
+
+def read_from_disk(filename: str) -> pd.DataFrame:
+    """Read csv file from disk"""
+    input_path = _file_path_generator(filename)
+    try:
+        return pd.read_csv(input_path)
+    except Exception as ex:
+        log.error(f'Faild on reading file in {input_path}, {ex}')
+        exit()
 
 
 def call_api(date: datetime, train_number: int = 5) -> pd.DataFrame:
@@ -53,6 +70,29 @@ def call_api(date: datetime, train_number: int = 5) -> pd.DataFrame:
             return full_day
     except Exception as ex:
         log.error(f'Faild on calling api on {date}, {ex}')
+
+
+def calc_avg_arrival_time(input_filename: str) -> str:
+    """
+    Read collected data from disk and return and calculate the average
+    actual arrival time at the final destination of the train during the
+    month January 2020.
+    """
+    df = read_from_disk(input_filename)
+    last_stops = df.groupby('departureDate')['scheduledTime'].max()
+    actualtimes = []
+    # extract arrival timess on final destinations
+    for index, value in last_stops.items():
+        actualtime = df[
+            (df['departureDate'] == index) &
+            (df['scheduledTime'] == value)]['actualTime'].values[0]
+        actualtimes.append((actualtime.split('T')[1]).split('.')[0])
+    # calculate the avg arrival time
+    avg = str(timedelta(
+        seconds=sum(
+            map(lambda f: int(f[0])*3600 + int(f[1])*60 + int(f[2]),
+                map(lambda f: f.split(':'), actualtimes))) / len(actualtimes)))
+    return avg.split('.')[0]
 
 
 if __name__ == '__main__':
